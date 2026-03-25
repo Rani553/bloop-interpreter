25aaf0cf4faadba6e233485ff0b267a632f27fcb
Flashrandom <gauravsinghrathorethakur@gmail.com>
2026-03-26 00:03:00 +0530

    Add abstract Expression base class with evaluate method

---
+++ src/Expression.java
@@ -0,0 +1,7 @@
+public interface Expression {
+    /**
+     * Evaluate this expression using the current variable store.
+     * Returns either a Double (for numbers) or a String (for text).
+     */
+    Object evaluate(Environment env);
+}
